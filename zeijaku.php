48b5a3c229b74938c3a81cd062ff9a6893264d98
takakuni <takakuni.shinnosuke@classmethod.jp>
2025-06-18 18:27:16 +0900

    Update zeijaku.php

--- zeijaku.php
+++ zeijaku.php
@@ -1,11 +1,4 @@
-<?php
-   $file = $_GET['file'];
-   if(isset($file))
-   {
-       include("pages/$file");
-   }
-   else
-   {
-       include("index.php");
-   }
-?>
+function nonCompliant(){
+    // Noncompliant: Does not ensures the request is being sent to the expected destination
+    $file = file_get_contents($_POST['r']);
+}
